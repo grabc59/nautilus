27e21f84857dfd876815dffdcb1cf069cc1401ba
Christopher Grabski <grabc59@gmail.com>
2017-02-27 13:48:18 -0700

    geomap shows up scaled in a card

--- public/app/parent/dashboard/geomap/geomap.component.js
+++ public/app/parent/dashboard/geomap/geomap.component.js
@@ -10,61 +10,39 @@
     function controller($stateParams, $state) {
         const vm = this;
         vm.$onInit = function() {
-            var dataset = [{
-                    "URL": "/route1",
-                    "count": 156
-                },
-                {
-                    "URL": "/index.html",
-                    "count": 30
-                },
-                {
-                    "URL": "/script.js",
-                    "count": 50
-                }
-            ];
-            var pie = d3.layout.pie()
-                .value(function(d) {
-                    return d.count
-                });
-            var color = d3.scale.category10();
-            var w = 300;
-            var h = 300;
-            var outerRadius = w / 3;
-            var innerRadius = w / 3 * .6;
+          // var w = 500;
+	        // var h = 300;
 
-            var arc = d3.svg.arc()
-                .innerRadius(innerRadius)
-                .outerRadius(outerRadius);
+          var margin = {top: 10, left: 10, bottom: 10, right: 10}
+          , width = parseInt(d3.select('#geomap').style('width'))
+          , width = width - margin.left - margin.right
+          , mapRatio = .5
+          , height = width * mapRatio;
 
-            var svg = d3.select("#geomap")
-                .append("svg")
-                .attr("width", w)
-                .attr("height", h);
-                // .style("background", "grey")
-            var arcs = svg.selectAll("g.arc")
-                .data(pie(dataset)) // pie-ify
-                .enter() // for each datum
-                .append("g") // create a g element
-                .attr("class", "arc") // which will be an arc
-                .attr("transform", "translate(" + outerRadius + ", " + outerRadius + ")"); // to size
+          console.log(width);
 
-            arcs.append("path")
-                .attr("fill", function(d, i) {
-                    return color(i); // generate pie arc color
-                })
-                .attr("d", arc);
+          var projection = d3.geo.albersUsa()
+              .scale(width)
+              .translate([width / 2, height / 2]);
 
-            arcs.append("text")
-                .attr("font-weight", "bold")
-                .attr("transform", function(d) {
-                    return "translate(" + arc.centroid(d) + ")";
-                })
-                .attr("text-anchor", "middle")
-                .attr("fill", "black")
-                .text(function(d) {
-                    return d.data.URL;
-                });
+          var path = d3.geo.path()
+              .projection(projection);
+
+          var svg = d3.select("#geomap")
+              .append("svg")
+              .attr("width", width)
+              .attr("height", height);
+          // var path = d3.geo.path();
+
+
+          d3.json("/assets/data/us-states.json", function(json) {
+
+          svg.selectAll("path")
+             .data(json.features)
+             .enter()
+             .append("path")
+             .attr("d", path);
+          });
         };
     }
 }());
