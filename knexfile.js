f05e6a2a0de1a5c9abd702789045524a685cc962
Christopher Grabski <grabc59@gmail.com>
2017-02-27 20:53:19 -0700

    add knex production config with DATABASE_URL

--- knexfile.js
+++ knexfile.js
@@ -7,5 +7,9 @@ module.exports = {
   test: {
     client: 'pg',
     connection: 'postgres://localhost/nautilus_test'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
 };
