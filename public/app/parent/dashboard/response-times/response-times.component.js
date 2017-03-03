9e3481db3cd9d4324cb64d02fdc118cc2529efac
Christopher Grabski <grabc59@gmail.com>
2017-03-03 13:40:27 -0700

    renamed top-ips to response-times component

--- public/app/parent/dashboard/response-times/response-times.component.js
+++ public/app/parent/dashboard/response-times/response-times.component.js
@@ -1,16 +1,16 @@
 (function() {
     'use strict';
     angular.module('app')
-        .component('topIps', {
+        .component('response', {
             controller: controller,
-            templateUrl: `app/parent/dashboard/top-ips/top-ips.template.html`
+            templateUrl: `app/parent/dashboard/response-times/response-times.template.html`
         });
     controller.$inject = ['$stateParams', '$state'];
 
     function controller($stateParams, $state) {
         const vm = this;
         vm.$onInit = function() {
-            
+
         };
     }
 }());
