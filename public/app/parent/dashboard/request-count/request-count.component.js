91afd4ad5b50ee19cb58ed983291421ecf64267e
Christopher Grabski <grabc59@gmail.com>
2018-11-05 21:59:51 -0800

    fixed the mini graphs moving around on mouseover

--- public/app/parent/dashboard/request-count/request-count.component.js
+++ public/app/parent/dashboard/request-count/request-count.component.js
@@ -1,198 +1,245 @@
 (function() {
-    'use strict';
-    angular.module('app')
-        .component('requestCount', {
-            controller: controller,
-            templateUrl: `app/parent/dashboard/request-count/request-count.template.html`
+  "use strict";
+  angular.module("app").component("requestCount", {
+    controller: controller,
+    templateUrl: `app/parent/dashboard/request-count/request-count.template.html`
+  });
+  controller.$inject = ["$stateParams", "$state"];
+
+  function controller($stateParams, $state) {
+    const vm = this;
+    vm.$onInit = function() {
+      d3.xhr("/logs", function(err, data) {
+        // created_at will look like this 2017-02-28T05:52:43.857Z
+        // parse the JSON
+        var parsedData = JSON.parse(data.responseText);
+        var parseTime = d3.timeParse("%Y-%m-%dT%H");
+        var dateOccurrences = {};
+        var d3DataArray = [];
+        // for tooltips - d3.bisector can find a value in an ordered array
+        var bisectDate = d3.bisector(function(d) {
+          return d.time;
+        }).left;
+
+        parsedData.forEach(function(element) {
+          // filter for each created_at field
+          let time = element.created_at;
+          let cleanedTime = time.slice(0, time.indexOf(":"));
+          // count occurances of each time
+          if (dateOccurrences.hasOwnProperty(cleanedTime)) {
+            dateOccurrences[cleanedTime]++;
+          } else {
+            dateOccurrences[cleanedTime] = 1;
+          }
         });
-    controller.$inject = ['$stateParams', '$state'];
 
-    function controller($stateParams, $state) {
-        const vm = this;
-        vm.$onInit = function() {
+        for (var i in dateOccurrences) {
+          let d3DataObj = {};
+          d3DataObj.time = parseTime(i);
+          d3DataObj.count = dateOccurrences[i];
+          d3DataArray.push(d3DataObj);
+        }
 
-            d3.xhr("/logs", function(err, data) {
-                // created_at will look like this 2017-02-28T05:52:43.857Z
-                // parse the JSON
-                var parsedData = JSON.parse(data.responseText)
-                var parseTime = d3.timeParse("%Y-%m-%dT%H");
-                var dateOccurrences = {}
-                var d3DataArray = [];
-                // for tooltips - d3.bisector can find a value in an ordered array
-                var bisectDate = d3.bisector(function(d) {
-                  return d.time;
-                }).left;
+        var padding = 50;
+        var width = 300;
+        var height = 200;
 
-                parsedData.forEach(function(element) {
-                    // filter for each created_at field
-                    let time = element.created_at;
-                    let cleanedTime = time.slice(0, time.indexOf(":"));
-                    // count occurances of each time
-                    if (dateOccurrences.hasOwnProperty(cleanedTime)) {
-                        dateOccurrences[cleanedTime]++;
-                    } else {
-                        dateOccurrences[cleanedTime] = 1;
-                    }
-                });
+        var svg = d3
+          .select("#request-count")
+          .append("svg")
+          .attr("width", "100%")
+          .attr("height", "100%")
+          .attr("viewBox", "0 0 " + width + " " + height)
+          .attr("preserveAspectRatio", "xMinYMin");
 
-                for (var i in dateOccurrences) {
-                    let d3DataObj = {}
-                    d3DataObj.time = parseTime(i);
-                    d3DataObj.count = dateOccurrences[i];
-                    d3DataArray.push(d3DataObj);
-                }
+        var x = d3.time
+          .scale()
+          .domain(
+            d3.extent(d3DataArray, function(d) {
+              return d.time;
+            })
+          )
+          .rangeRound([padding, width - padding]);
 
-                var padding = 50;
-                var width = 300
-                var height = 200
+        var y = d3.scale
+          .linear()
+          .domain(
+            d3.extent(d3DataArray, function(d) {
+              return d.count;
+            })
+          )
+          .rangeRound([height - padding, padding]);
 
-                var svg = d3.select("#request-count")
-                    .append("svg")
-                    .attr("width", '100%')
-                    .attr("height", '100%')
-                    .attr("viewBox", "0 0 " + width + " " + height)
-                    .attr('preserveAspectRatio', 'xMinYMin')
+        var xAxis = d3.svg
+          .axis()
+          .scale(x)
+          .orient("bottom")
+          .ticks(d3.time.days, 1)
+          .ticks(3);
 
-                var x = d3.time.scale()
-                    .domain(d3.extent(d3DataArray, function(d) {
-                        return d.time;
-                    }))
-                    .rangeRound([padding, width - padding]);
+        svg
+          .append("g")
+          .attr("class", "axis x-axis")
+          .attr("transform", "translate(0," + (height - padding) + ")")
+          .transition()
+          .duration(1000)
+          .call(xAxis);
 
-                var y = d3.scale.linear()
-                    .domain(d3.extent(d3DataArray, function(d) {
-                        return d.count;
-                    }))
-                    .rangeRound([height - padding, padding]);
+        var yAxis = d3.svg
+          .axis()
+          .scale(y)
+          .orient("left")
+          .tickFormat(function(d) {
+            return d;
+          })
+          .tickSize(5, 5, 0)
+          .ticks(5); // set rough # of ticks
 
-                var xAxis = d3.svg.axis()
-                    .scale(x)
-                    .orient("bottom")
-                    .ticks(d3.time.days, 1)
-                    .ticks(3)
+        svg
+          .append("g")
+          .attr("class", "axis y-axis")
+          .attr("transform", "translate(" + padding + ",0)")
+          .transition()
+          .duration(1000)
+          .call(yAxis);
 
-                svg.append("g")
-                    .attr("class", "axis x-axis")
-                    .attr("transform", "translate(0," + (height - padding) + ")")
-                    .transition()
-                    .duration(1000)
-                    .call(xAxis);
+        /////////////////////
+        /////// THE LINE
+        /////////////////////
 
-                var yAxis = d3.svg.axis()
-                    .scale(y)
-                    .orient("left")
-                    .tickFormat(function(d) {
-                        return d;
-                    })
-                    .tickSize(5, 5, 0)
-                    .ticks(5); // set rough # of ticks
+        var valueline = d3.svg
+          .line()
+          .interpolate("monotone")
+          .x(function(d) {
+            return x(d.time);
+          })
+          .y(function(d) {
+            return y(d.count);
+          });
 
-                svg.append("g")
-                    .attr("class", "axis y-axis")
-                    .attr("transform", "translate(" + padding + ",0)")
-                    .transition()
-                    .duration(1000)
-                    .call(yAxis);
+        var lineSvg = svg.append("g");
+        var path = lineSvg
+          .append("path")
+          .attr("class", "line")
+          .attr("d", valueline(d3DataArray));
 
+        var totalLength = path.node().getTotalLength();
 
+        path
+          .attr("stroke-dasharray", totalLength + " " + totalLength)
+          .attr("stroke-dashoffset", totalLength)
+          .transition()
+          .duration(1000)
+          .ease("linear")
+          .attr("stroke-dashoffset", 0);
 
-                /////////////////////
-                /////// THE LINE
-                /////////////////////
+        /////////////////////
+        /////// TOOLTIPS
+        /////////////////////
 
-                var valueline = d3.svg.line()
-                    .interpolate("monotone")
-                    .x(function(d) { return x(d.time); })
-                    .y(function(d) { return y(d.count); });
+        /////// Adding the circle to the graph
+        var focus = svg.append("g").style("display", "none");
 
-                var lineSvg = svg.append("g");
-                var path = lineSvg.append("path")
-                  .attr("class", "line")
-                  .attr("d", valueline(d3DataArray));
+        focus
+          .append("circle")
+          .attr("class", "y")
+          .style("fill", "none")
+          .style("stroke", "blue")
+          .attr("r", 4);
 
-                var totalLength = path.node().getTotalLength();
+        focus
+          .append("div")
+          .attr("class", "tooltip")
+          .attr("id", "request-count-tooltip");
 
-                path
-                .attr("stroke-dasharray", totalLength + " " + totalLength)
-                .attr("stroke-dashoffset", totalLength)
-                .transition()
-                .duration(1000)
-                .ease("linear")
-                .attr("stroke-dashoffset", 0);
+        focus
+          .append("text")
+          .attr("x", 15)
+          .attr("dy", ".31em");
 
-                /////////////////////
-                /////// TOOLTIPS
-                /////////////////////
+        /////// CREATE TOOLTIP
+        var requestCountTooltip = d3
+          .select("request-count")
+          .append("div")
+          .attr("class", "tooltip")
+          .attr("id", "request-count-tooltip");
 
-                /////// Adding the circle to the graph
-                var focus = svg.append("g")
-                    .style("display", "none");
+        /////// Set the area that we use to capture our mouse movements
+        svg
+          .append("rect")
+          .attr("width", "100%")
+          .attr("height", "100%")
+          .style("fill", "none")
+          .style("pointer-events", "all")
+          .on("mouseover", function() {
+            focus.style("display", null);
+          })
+          .on("mouseout", function() {
+            requestCountTooltip.style("display", "none");
+            focus.style("display", "none");
+          })
+          .on("mousemove", mousemove);
 
-                focus.append("circle")
-                        .attr("class", "y")
-                        .style("fill", "none")
-                        .style("stroke", "blue")
-                        .attr("r", 4);
+        function mousemove() {
+          /////// DETERMINING WHICH DATE WILL BE HIGHLIGHTED
+          // determine the mouse's x coordinate
+          var x0 = x.invert(d3.mouse(this)[0]),
+            // find the index of the x axis value (date) closest to the mouse
+            i = bisectDate(d3DataArray, x0, 1),
+            // the value pair to the left of the mouse
+            d0 = d3DataArray[i - 1],
+            // the value pair to the right of the mouse
+            d1 = d3DataArray[i],
+            // the value pair closest to the mouse
+            compareDateValues = function(leftVal, rightVal, mouseXPosition) {
+              // check if values are defined
+              if (leftVal && rightVal) {
+                return mouseXPosition - leftVal.time >
+                  rightVal.time - mouseXPosition
+                  ? rightVal
+                  : leftVal;
+              } else {
+                return; // prevent console logs errors when mouse is at the edge of the line graph
+              }
+            },
+            d = compareDateValues(d0, d1, x0);
 
+          ////// move the circle to the position of the mouse
 
-                focus.append("div")
-                    .attr("class", "tooltip")
-                    .attr("id", "request-count-tooltip")
+          // if a mouse position between 2 values was determined
+          if (d) {
+            focus
+              .select("circle.y")
+              .attr(
+                "transform",
+                "translate(" + x(d.time) + "," + y(d.count) + ")"
+              );
 
-                /////// CREATE TOOLTIP
-                var requestCountTooltip = d3.select("request-count").append("div").attr("class", "tooltip").attr("id", "request-count-tooltip");
+            focus
+              .select("text")
+              .text(function() {
+                return d.count;
+              })
+              .attr(
+                "transform",
+                "translate(" + x(d.time) + "," + y(d.count) + ")"
+              );
 
-                /////// Set the area that we use to capture our mouse movements
-                svg.append("rect")
-                .attr("width", '100%')
-                .attr("height", '100%')
-                   .style("fill", "none")
-                   .style("pointer-events", "all")
-                   .on("mouseover", function() { focus.style("display", null); })
-                   .on("mouseout", function() {
-                     requestCountTooltip.style("display", "none");
-                     focus.style("display", "none");
-                   })
-                   .on("mousemove", mousemove);
-
-                   function mousemove() {
-                     /////// DETERMINING WHICH DATE WILL BE HIGHLIGHTED
-                      // determine the mouse's x coordinate
-                       var x0 = x.invert(d3.mouse(this)[0]),
-                        // find the index of the x axis value (date) closest to the mouse
-                          i = bisectDate(d3DataArray, x0, 1),
-                          // the value pair to the left of the mouse
-                          d0 = d3DataArray[i - 1],
-                          // the value pair to the right of the mouse
-                          d1 = d3DataArray[i],
-                          // the value pair closest to the mouse
-                          compareDateValues = function (leftVal, rightVal, mouseXPosition) {
-                            // check if values are defined
-                            if (leftVal && rightVal) {
-                              return mouseXPosition - leftVal.time > rightVal.time - mouseXPosition ? rightVal : leftVal;
-                            } else {
-                              return; // prevent console logs errors when mouse is at the edge of the line graph
-                            }
-                          },
-                          d = compareDateValues(d0, d1, x0);
-
-                      ////// move the circle to the position of the mouse
-
-                      // if a mouse position between 2 values was determined
-                      if (d) {
-                       focus.select("circle.y")
-                           .attr("transform",
-                              "translate(" + x(d.time) + "," + y(d.count) + ")");
-
-                        requestCountTooltip.style("left", d3.event.pageX+10+"px");
-                        requestCountTooltip.style("top", d3.event.pageY-25+"px");
-                        // console.log(requestCountTooltip.style("left", d3.event.pageX+10+"px"));
-                        requestCountTooltip.style("display", "inline-block");
-                        requestCountTooltip.style("word-wrap", "break-word");
-                        requestCountTooltip.html("<strong>URL: </strong>" + (d.time)+"<br>"+"<strong>Count: </strong>" + (d.count));
-                      }
-                   }
-            });
-        };
-    }
-}());
+            // requestCountTooltip.style("left", d3.event.pageX + 10 + "px");
+            // requestCountTooltip.style("top", d3.event.pageY - 25 + "px");
+            // // // console.log(requestCountTooltip.style("left", d3.event.pageX+10+"px"));
+            // requestCountTooltip.style("display", "inline-block");
+            // requestCountTooltip.style("word-wrap", "break-word");
+            // requestCountTooltip.html(
+            //   "<strong>URL: </strong>" +
+            //     d.time +
+            //     "<br>" +
+            //     "<strong>Count: </strong>" +
+            //     d.count
+            // );
+          }
+        }
+      });
+    };
+  }
+})();
--- public/app/parent/dashboard/top-routes/top-routes.component.js
+++ public/app/parent/dashboard/top-routes/top-routes.component.js
@@ -1,155 +1,176 @@
 (function() {
-    'use strict';
-    angular.module('app')
-        .component('topRoutes', {
-            controller: controller,
-            templateUrl: `app/parent/dashboard/top-routes/top-routes.template.html`
-        });
-    controller.$inject = ['$stateParams', '$state'];
+  "use strict";
+  angular.module("app").component("topRoutes", {
+    controller: controller,
+    templateUrl: `app/parent/dashboard/top-routes/top-routes.template.html`
+  });
+  controller.$inject = ["$stateParams", "$state"];
 
-    function controller($stateParams, $state) {
-        const vm = this;
+  function controller($stateParams, $state) {
+    const vm = this;
 
-        vm.$onInit = function() {
-
-          ////////////////////////////
-          /////// HELPER FUNCTIONS
-          ////////////////////////////
-          /////// count occurances of each item in the requested data
-          function countDataOccurances(jsonData, property1) {
-            var occurancesObj = {};
-            var array = JSON.parse(jsonData.responseText);
-            for (var i = 0; i < array.length; i++) {
-              if (occurancesObj.hasOwnProperty(array[i][property1])) {
-                occurancesObj[array[i][property1]]++;
-              } else {
-                occurancesObj[array[i][property1]] = 1;
-              }
-            }
-            return occurancesObj
+    vm.$onInit = function() {
+      ////////////////////////////
+      /////// HELPER FUNCTIONS
+      ////////////////////////////
+      /////// count occurances of each item in the requested data
+      function countDataOccurances(jsonData, property1) {
+        var occurancesObj = {};
+        var array = JSON.parse(jsonData.responseText);
+        for (var i = 0; i < array.length; i++) {
+          if (occurancesObj.hasOwnProperty(array[i][property1])) {
+            occurancesObj[array[i][property1]]++;
+          } else {
+            occurancesObj[array[i][property1]] = 1;
           }
-          /////// prep the data count so D3 can easily read it
-          function convertDataCountObjForD3(dataOccurances, propertyBeingCounted, resultPropertyName) {
-            var d3DataArray = [];
-            for (var i in dataOccurances) {
-              let d3DataObj = {}
-              d3DataObj[propertyBeingCounted] = i;
-              d3DataObj[resultPropertyName] = dataOccurances[i];
-              d3DataArray.push(d3DataObj);
-            }
-            // cities.sort(function(x, y){
-            //    return d3.ascending(x.index, y.index);
-            // })
-            // d3DataArray.sort(function(x, y) {
-            //   return d3.descending(x.)
-            // })
-            return d3DataArray;
-          }
+        }
+        return occurancesObj;
+      }
+      /////// prep the data count so D3 can easily read it
+      function convertDataCountObjForD3(
+        dataOccurances,
+        propertyBeingCounted,
+        resultPropertyName
+      ) {
+        var d3DataArray = [];
+        for (var i in dataOccurances) {
+          let d3DataObj = {};
+          d3DataObj[propertyBeingCounted] = i;
+          d3DataObj[resultPropertyName] = dataOccurances[i];
+          d3DataArray.push(d3DataObj);
+        }
+        // cities.sort(function(x, y){
+        //    return d3.ascending(x.index, y.index);
+        // })
+        // d3DataArray.sort(function(x, y) {
+        //   return d3.descending(x.)
+        // })
+        return d3DataArray;
+      }
 
-          /////// CREATE TOOLTIP
-          var topRoutesTooltip = d3.select("body").append("div").attr("class", "tooltip").attr("id", "top-routes-tooltip");
+      /////// CREATE TOOLTIP
+      var topRoutesTooltip = d3
+        .select("body")
+        .append("div")
+        .attr("class", "tooltip")
+        .attr("id", "top-routes-tooltip");
 
-          /////// REQUEST DATA
-          d3.xhr("/logs/top-routes", function(err, data) {
+      /////// REQUEST DATA
+      d3.xhr("/logs/top-routes", function(err, data) {
+        ////// PREP REQUESTED DATA FOR D3
+        var d3RouteData = convertDataCountObjForD3(
+          countDataOccurances(data, "url"),
+          "url",
+          "count"
+        )
+          .sort(function(x, y) {
+            return d3.descending(x.count, y.count);
+          })
+          .slice(0, 10);
+        // console.log(d3RouteData);
 
-            ////// PREP REQUESTED DATA FOR D3
-            var d3RouteData = convertDataCountObjForD3(countDataOccurances(data, "url"), "url", "count" )
-            .sort(function(x, y) {
-              return d3.descending(x.count, y.count);
-            })
-            .slice(0,10);
-            // console.log(d3RouteData);
-
-            ////////////////////////////
-            /////// DRAW THE PIE CHART
-            ////////////////////////////
-            var topRoutesPie = d3.layout.pie()
-                // .startAngle(1.1*Math.PI)
-                // .endAngle(3.1*Math.PI)
-                .value(function(d) {
-                    return d.count
-                });
-
-            var colorRange = d3.scale.category20c();
-            var color = d3.scale.ordinal()
-            	.range(colorRange.range());
-
-            var w = 300
-            var h = 200
-            var padding = 20;
-
-            var outerRadius = Math.min(w,h)/2 - padding;
-            // var innerRadius = outerRadius * .5;
-
-            var arc = d3.svg.arc()
-                // .innerRadius(innerRadius)
-                .outerRadius(outerRadius);
-
-            // for expanding pie pieces
-            var arcOver = d3.svg.arc()
-              .outerRadius(outerRadius + 18)
-              // .innerRadius(innerRadius)
-
-            var topRoutesSvg = d3.select("#top-routes")
-                .append("svg")
-                .attr("width", '50%')
-                .attr("height", '80%')
-                .attr("viewBox", "0 0 " + Math.min(w,h) + " " + Math.min(w,h))
-                .attr('preserveAspectRatio','xMinYMin')
-
-            var g = topRoutesSvg
-                .append('g')
-                .attr('transform', 'translate(' + padding +  ',' + padding + ')');
-
-            var arcs = g.selectAll("g.arc")
-                .data(topRoutesPie(d3RouteData)) // pie-ify
-                .enter() // for each datum
-                .append("g") // create a g element
-                .attr("class", "arc") // which will be an arc
-                .attr("transform", "translate(" + outerRadius + ", " + outerRadius + ")"); // to size
-
-            arcs.append("path")
-                .attr("fill", function(d, i) {
-                    return color(d.data.url); // generate pie arc color
-                })
-                .on("mouseenter", function(d) {
-                  d3.select(this)
-                     .transition()
-                     .duration(100)
-                     .attr("d", arcOver)
-                })
-
-                .on("mouseout", function(d){
-                    // remove tooltip
-                    topRoutesTooltip.style("display", "none");
-
-                    // expading pie pieces
-                    d3.select(this).transition()
-                       .attr("d", arc)
-                       .attr("stroke","none");
-                })
-                .transition()
-                  .ease("exp")
-                  .duration(1500)
-                  .attrTween("d", tweenPie)
-
-            /////// CHART LABELS
-            arcs.on("mousemove", function(d){
-                topRoutesTooltip.style("left", d3.event.pageX+10+"px");
-                topRoutesTooltip.style("top", d3.event.pageY-25+"px");
-                topRoutesTooltip.style("display", "inline-block");
-                topRoutesTooltip.style("word-wrap", "break-word");
-                topRoutesTooltip.html("<strong>URL: </strong>" + (d.data.url)+"<br>"+"<strong>Count: </strong>" + (d.data.count))
-
-
-            });
-
-            function tweenPie(b) {
-              var i = d3.interpolate({startAngle: 0, endAngle: 0}, b);
-              return function(t) { return arc(i(t)); };
-            }
-
+        ////////////////////////////
+        /////// DRAW THE PIE CHART
+        ////////////////////////////
+        var topRoutesPie = d3.layout
+          .pie()
+          // .startAngle(1.1*Math.PI)
+          // .endAngle(3.1*Math.PI)
+          .value(function(d) {
+            return d.count;
           });
-        };
-    }
-}());
+
+        var colorRange = d3.scale.category20c();
+        var color = d3.scale.ordinal().range(colorRange.range());
+
+        var w = 300;
+        var h = 200;
+        var padding = 20;
+
+        var outerRadius = Math.min(w, h) / 2 - padding;
+        // var innerRadius = outerRadius * .5;
+
+        var arc = d3.svg
+          .arc()
+          // .innerRadius(innerRadius)
+          .outerRadius(outerRadius);
+
+        // for expanding pie pieces
+        var arcOver = d3.svg.arc().outerRadius(outerRadius + 18);
+        // .innerRadius(innerRadius)
+
+        var topRoutesSvg = d3
+          .select("#top-routes")
+          .append("svg")
+          .attr("width", "50%")
+          .attr("height", "80%")
+          .attr("viewBox", "0 0 " + Math.min(w, h) + " " + Math.min(w, h))
+          .attr("preserveAspectRatio", "xMinYMin");
+
+        var g = topRoutesSvg
+          .append("g")
+          .attr("transform", "translate(" + padding + "," + padding + ")");
+
+        var arcs = g
+          .selectAll("g.arc")
+          .data(topRoutesPie(d3RouteData)) // pie-ify
+          .enter() // for each datum
+          .append("g") // create a g element
+          .attr("class", "arc") // which will be an arc
+          .attr(
+            "transform",
+            "translate(" + outerRadius + ", " + outerRadius + ")"
+          ); // to size
+
+        arcs
+          .append("path")
+          .attr("fill", function(d, i) {
+            return color(d.data.url); // generate pie arc color
+          })
+          .on("mouseenter", function(d) {
+            d3.select(this)
+              .transition()
+              .duration(100);
+            //  .attr("d", arcOver)
+          })
+
+          .on("mouseout", function(d) {
+            // remove tooltip
+            topRoutesTooltip.style("display", "none");
+
+            // expading pie pieces
+            d3.select(this)
+              .transition()
+              .attr("d", arc)
+              .attr("stroke", "none");
+          })
+          .transition()
+          .ease("exp")
+          .duration(1500)
+          .attrTween("d", tweenPie);
+
+        /////// CHART LABELS
+        arcs.on("mousemove", function(d) {
+          topRoutesTooltip.style("left", d3.event.pageX + 10 + "px");
+          topRoutesTooltip.style("top", d3.event.pageY - 25 + "px");
+          topRoutesTooltip.style("display", "inline-block");
+          topRoutesTooltip.style("word-wrap", "break-word");
+          topRoutesTooltip.html(
+            "<strong>URL: </strong>" +
+              d.data.url +
+              "<br>" +
+              "<strong>Count: </strong>" +
+              d.data.count
+          );
+        });
+
+        function tweenPie(b) {
+          var i = d3.interpolate({ startAngle: 0, endAngle: 0 }, b);
+          return function(t) {
+            return arc(i(t));
+          };
+        }
+      });
+    };
+  }
+})();
